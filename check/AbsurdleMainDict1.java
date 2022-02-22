679565b74b119c4a4358a618662941cd7df6f3a0
Kevin Lin <kevinl@cs.uw.edu>
2022-02-22 09:58:54 -0800

    Add check

---
+++ check/AbsurdleMainDict1.java
@@ -0,0 +1,56 @@
+// Class AbsurdleMain is the driver program for the Absurdle program. It reads
+// a dictionary of words to be used during the game and then plays a game with
+// the user. You can change the setting for SHOW_COUNT to see how many options
+// are still left on each turn.
+
+import java.util.*;
+import java.io.*;
+
+public class AbsurdleMainDict1  {
+    public static final String DICTIONARY_FILE = "dictionary1.txt";
+    public static final int LENGTH = 5;
+    public static final boolean SHOW_COUNT = true;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // open the dictionary file and read dictionary into an ArrayList
+        Scanner input = new Scanner(new File(DICTIONARY_FILE));
+        List<String> dictionary = new ArrayList<String>();
+        while (input.hasNext()) {
+            dictionary.add(input.next().toLowerCase());
+        }
+
+        // set up the AbsurdleManager and start the game
+        List<String> dictionary2 = Collections.unmodifiableList(dictionary);
+        AbsurdleManager absurdle = new AbsurdleManager(dictionary2, LENGTH);
+        if (absurdle.words().isEmpty()) {
+            System.out.println("No words of that length in the dictionary.");
+        } else {
+            Scanner console = new Scanner(System.in);
+            List<String> patterns = new ArrayList<>();
+            while (!isFinished(patterns)) {
+                if (SHOW_COUNT) {
+                    System.out.println(absurdle.words().size() + " words");
+                }
+                System.out.print("> ");
+                String guess = console.next().toLowerCase().strip();
+                String pattern = absurdle.record(guess);
+                if (!pattern.isEmpty()) {
+                    patterns.add(pattern);
+                    System.out.println(": " + pattern);
+                    System.out.println();
+                }
+            }
+            System.out.println("Absurdle " + patterns.size() + "/∞");
+            System.out.println();
+            System.out.println(String.join("\n", patterns));
+        }
+    }
+
+    private static boolean isFinished(List<String> patterns) {
+        if (patterns.isEmpty()) {
+            return false;
+        }
+        String lastPattern = patterns.get(patterns.size() - 1);
+        return !lastPattern.contains("⬜") && !lastPattern.contains("🟨");
+    }
+}
---
+++ check/compare.py
@@ -0,0 +1,78 @@
+import contextlib
+import icdiff
+import pexpect
+import subprocess
+import sys
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class Options:
+    # Facade data class for argparse options
+    is_git_diff: bool = False
+    labels: str = ""
+    no_headers: bool = False
+    head: int = 0
+    matcher: str = ""
+    no_bold: bool = False
+    color_map: str = ""
+    whole_file: bool = False
+    output_encoding: str = "utf-8"
+    unified: int = 5
+    strip_trailing_cr: bool = True
+
+
+def run(path, class_name):
+    result = []
+    try:
+        absurdle = pexpect.spawn(f"java {class_name}.java", timeout=3)
+        with open(path) as f:
+            for line in f.readlines():
+                absurdle.expect_exact("> ")
+                result.append(absurdle.before.decode("utf-8"))
+                result.append("> ")
+                absurdle.send(line)
+        absurdle.expect_exact(pexpect.EOF)
+    except (pexpect.TIMEOUT, pexpect.EOF):
+        pass
+    finally:
+        result.append(absurdle.before.decode("utf-8"))
+        return "".join(result)
+
+
+if __name__ == "__main__":
+    options = Options()
+    icdiff.set_cols_option(options)
+    cd = icdiff.ConsoleDiff(
+        cols=options.cols,
+        strip_trailing_cr=options.strip_trailing_cr
+    )
+
+    def diff(expected, actual, expected_header, actual_header):
+        printed_lines = 0
+        for line in cd.make_table(
+                expected.splitlines(), actual.splitlines(),
+                expected_header, actual_header,
+                context=(not options.whole_file),
+                numlines=int(options.unified)
+            ):
+            icdiff.codec_print(line, options)
+            sys.stdout.flush()
+            printed_lines += 1
+        return printed_lines != 1
+
+    err = 0
+    for p in sorted(Path("dict2").glob("*.input")):
+        with open(p.with_suffix(".expected")) as f:
+            if diff(f.read(), run(p.with_suffix(".input"), "AbsurdleMainDict2"),
+                    p.with_suffix(".expected"), p.with_suffix(".actual")):
+                err = 1
+
+    for p in sorted(Path("dict1").glob("*.input")):
+        with open(p.with_suffix(".expected")) as f:
+            if diff(f.read(), run(p.with_suffix(".input"), "AbsurdleMainDict1"),
+                    p.with_suffix(".expected"), p.with_suffix(".actual")):
+                err = 1
+
+    sys.exit(err)
